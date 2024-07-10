22bc5488fd0e5639686e2a74ee3e9f1d416b484e
lotaxgizmo <lotaoko2@gmail.com>
2024-07-10 15:38:58 +0100

    new commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,25 +13,25 @@ function Home() {
             <div className='flex flex-col lg:flex-row items-start justify-center relative lg:px-0 p-4'>
 
 
-                <div className="pido relative flex flex-col justify-center items-center lg:items-start ">
+                <div className="pido relative flex flex-col justify-center items-start lg:items-start ">
                     <img src={herobgoverlay} alt="" className='-z-10 absolute lg:-top-32' />
-                    <img src={herotxt} alt="" />
+                    <img src={herotxt} alt="" className='w-72 lg:w-auto' />
 
-                    <p className='lg:text-3xl my-2 lg:text-left text-center lg:pl-3 w-[750px]'>
+                    <p className='lg:text-3xl my-2  lg:text-left text-left lg:pl-0 w-80 lg:w-[750px]'>
                         PIDO, THE TWO SWEETEST THINGS IN THE WORLD HAS JOINED FORCES TO BECOME SOMETHING EVEN SWEETER.
                     </p>
-                    <p className='lg:text-3xl my-2 lg:text-left text-center lg:pl-3 w-[800px] text-[#EE3B23]'>
+                    <p className='lg:text-3xl my-2 lg:text-left text-left lg:pl-0 w-[800px] text-[#EE3B23]'>
                         CONTRACT ADDRESS
                     </p>
 
 
-                    <img src={contractadd} alt="" className='m-2 w-10/12 lg:w-auto' />
+                    <img src={contractadd} alt="" className='ml-0 m-2 w-5/12  lg:w-auto' />
                     <div className="btn flex flex-col lg:flex-row ">
                         <a href="#" target="_blank" rel="noopener noreferrer">
-                            <img src={buypidocoinbtn} alt="" className='m-2 w-60 lg:w-auto' />
+                            <img src={buypidocoinbtn} alt="" className='ml-0 m-2 w-60 lg:w-auto' />
                         </a>
                         <a href="#" target="_blank" rel="noopener noreferrer">
-                            <img src={raydiumbtn} alt="" className='m-2 w-60 lg:w-auto' />
+                            <img src={raydiumbtn} alt="" className='ml-0 m-2 w-60 lg:w-auto' />
                         </a>
                     </div>
                 </div>
--- src/components/Pidonomics.js
+++ src/components/Pidonomics.js
@@ -5,14 +5,31 @@ import tokenomics from '../assets/tokenomics.png'
 import totaltxt from '../assets/totaltxt.png'
 import bannerimg from '../assets/bannerimg.svg'
 
+import tk5box from '../assets/tk5box.png'
+import tk4box from '../assets/tk4box.png'
+import tk3box from '../assets/tk3box.png'
+import tk2box from '../assets/tk2box.png'
+import tk1box from '../assets/tk1box.png'
+import tokebg from '../assets/tokebg.png'
+
 function Pidonomics() {
     return (
         <>
-            <div className='flex flex-col items-center justify-center relative p-4'>
+            <div className=' flex flex-col items-center justify-center relative p-4'>
                 <img src={pidonomicstxt} alt="" className='lol my-3' />
                 <img src={pidonomics2txt} alt="" className='lol my-3' />
                 <img src={totaltxt} alt="" className='lol my-3' />
-                <img src={tokenomics} alt="" className='lol my-3' />
+                <img src={tokenomics} alt="" className='lol my-3 hidden lg:flex' />
+
+
+                <div className="toke block lg:hidden">
+                    <img src={tk1box} alt="" className='w-96 my-3' />
+                    <img src={tk2box} alt="" className='w-96 my-3' />
+                    <img src={tk3box} alt="" className='w-96 my-3' />
+                    <img src={tk4box} alt="" className='w-96 my-3' />
+                    <img src={tk5box} alt="" className='w-96 my-3' />
+                </div>
+                <img src={tokebg} alt="" className='absolute -z-10 top-0 w-full mix-blend-overlay flex lg:hidden' />
             </div>
             <img src={bannerimg} alt="" className='w-full' />
         </>
